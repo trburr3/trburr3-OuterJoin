3ef67b13057c9bd4d15b45dbd262df3f48d5c592
Tre B <burrowstrevon@gmail.com>
2025-05-29 23:15:58 +0000

    complete

---
+++ problem1.sql
@@ -0,0 +1,1 @@
+SELECT * FROM class LEFT  JOIN student ON class.class_title = student.class_title;
---
+++ problem2.sql
@@ -0,0 +1,1 @@
+SELECT * FROM class RIGHT JOIN textbook ON class.class_title = textbook.class_title;
